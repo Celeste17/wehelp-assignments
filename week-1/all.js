1748455d26e8f104d214822eb55075f12140fba0
Celeste17 <89852805+Celeste17@users.noreply.github.com>
2021-12-30 23:49:50 +0800

    Add files via upload

---
+++ week-1/all.js
@@ -0,0 +1,12 @@
+// const menu = document.querySelector(".headerMenu");
+// const showmenu = document.querySelector("#showMenu");
+
+// showmenu.addEventListener("click", mobileMenu);
+
+// function mobileMenu() {
+//     console.log("correct")
+//     menu.classList.toggle("active")
+// }
+
+
+
